905c16c82e00cc73ab1824658644c89ed449c763
Sebastien Renauld <seb.renauld@gmail.com>
2018-11-21 13:22:11 +0100

    Update error.rs

--- src/posix/error.rs
+++ src/posix/error.rs
@@ -1,22 +1,6 @@
-#[cfg(all(target_os = "linux", not(target_env = "musl"), feature = "libudev"))]
-use libudev;
 use nix;
 
 use std::io;
-
-#[cfg(all(target_os = "linux", not(target_env = "musl"), feature = "libudev"))]
-impl From<libudev::Error> for ::Error {
-    fn from(e: libudev::Error) -> ::Error {
-        let description = e.description().to_string();
-        match e.kind() {
-            libudev::ErrorKind::NoMem => ::Error::new(::ErrorKind::Unknown, description),
-            libudev::ErrorKind::InvalidInput => {
-                ::Error::new(::ErrorKind::InvalidInput, description)
-            }
-            libudev::ErrorKind::Io(a) => ::Error::new(::ErrorKind::Io(a), description),
-        }
-    }
-}
 
 impl From<nix::Error> for ::Error {
     fn from(e: nix::Error) -> ::Error {
